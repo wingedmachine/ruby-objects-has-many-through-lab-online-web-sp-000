20b1af165acdd2340a2feb4bd4412daa46be2195
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-24 01:16:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,11 @@
 class Song
-  attr_reader :name, :genre
+  attr_reader :name, :genre, :artist
   def initialize(name, genre)
     @name = name
     @genre = genre
+  end
+
+  def add_artist(artist)
+    @artist = artist
   end
 end
